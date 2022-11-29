a2deb4992bc263fd1ac56f93f143bea662010d72
alexandre <alexandre.grandon@epita.fr>
2022-11-29 01:10:18 +0100

    space problem

--- src/shell.cpp
+++ src/shell.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unistd.h>
-//#include <sys/wait.h>
+#include <sys/wait.h>
 #include <string>
 #include <stdlib.h>
 #include <stdio.h>
@@ -34,11 +34,8 @@ std::vector<std::vector<std::string>> ash_splite_line(std::vector<char> line){
     while(i < std::size(line)){
         if(line[i] == ';'){
             Tokens.push_back(token);
-            //	printf("%s\n", Tokens[bpos]);
             token.clear();
-            //	printf("%s\n", Tokens[bpos]);
             all_lines.push_back(Tokens);
-            //	printf("%s\n", all_lines[0][bpos]);
             Tokens.clear();
         }
         else if(line[i] == ' '){
@@ -46,18 +43,60 @@ std::vector<std::vector<std::string>> ash_splite_line(std::vector<char> line){
             token.clear();
         }
 
-        else{
+        else if(line[i] != '\0'){
             token.push_back(line[i]);
         }
         i++;
     }
-    Tokens.push_back(token);
+    std::cout << std::size(token) << "  " << std::size(Tokens) << std::endl;
+    if(std::size(token)>=1){
+    	Tokens.push_back(token);
+    }
     all_lines.push_back(Tokens);
-    printf("%s\n", all_lines[0][0].c_str());
+    printf(">>%s<<\n", all_lines[0][0].c_str());
     return all_lines;
 }
 
 
+
+bool ash_launch(char** args){
+	pid_t pid, wpid;
+	int status;
+	pid = fork();
+	if(pid == 0){
+		printf(">>%s<<\n", args[0]);
+		if(execvp(args[0], args) == -1){
+			printf("ash: command not found\n");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if(pid < 0){
+		perror("ash");
+	}
+	else{
+		do{
+			wpid = waitpid(pid, &status, WUNTRACED);
+		}while(!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return true;
+}
+
+
+bool ash_execute(std::vector<std::vector<std::string>> commande){
+	bool status = true;
+	for(size_t i = 0; i < std::size(commande); i++){
+		char** args = (char**)malloc(sizeof(char*) * std::size(commande[i]));
+		for(size_t j = 0; j < std::size(commande[i]); j++){
+			args[j] = reinterpret_cast<char*>(commande[i][j].data());
+		}
+		status = status && ash_launch(args);
+	}
+	
+	return status;
+}	
+
+
+		
 
 void ash_loop(void){
     std::vector<char> line;
@@ -68,8 +107,7 @@ void ash_loop(void){
         printf("$");
         line = ash_readline();
         args = ash_splite_line(line);
-        std::cout << std::size(args) << std::endl;
-        printf("herer >> %s << \n", args[0][0].c_str());
+	status = ash_execute(args);
 
 
     }while(status);
